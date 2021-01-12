5854fec9f1f88a5621e3b77521bca3e61193bf5c
Chauseth <alexandre.chiquet@hotmail.com>
2021-01-12 14:23:18 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class Header extends Component {
+
+
+    render() {
+        return (
+          <div className="Header bg-success">
+            <h1>Mon titre</h1>
+          </div>
+        );
+      }
+      
+}
+
+export default Header;
