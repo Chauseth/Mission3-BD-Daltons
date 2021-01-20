08bccb935717746c36b4b4dbc9613bf48f122508
Chauseth <alexandre.chiquet@hotmail.com>
2021-01-20 10:58:12 +0100

    Correction bug Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,27 +3,12 @@ import Form from "react-bootstrap/Form";
 import { useState } from "react";
 import Filters from "./Filters";
 
-const Search = props => {
+const Search = (props) => {
   const [input, setInput] = useState("");
   const [authorInput, setAuthorInput] = useState("");
 
   return (
     <div className="BDS d-flex flex-column flex-wrap">
-<<<<<<< HEAD
-      <Form className="formclass d-flex justify-content-center ">
-        <Form.Group className="d-flex flex-column flex-md-row">
-          <input
-            placeholder="Titre"
-            className="text-center mb-3"
-            onChange={e => setInput(e.target.value)}
-          />
-          <input
-            placeholder="Auteur"
-            className="text-center mb-3"
-            onChange={e => setAuthorInput(e.target.value)}
-          />
-        </Form.Group>
-=======
       <Form className="formclass d-flex  flex-column mx-auto mt-3 justify-content-center ">
         <input
           placeholder="Titre/Série"
@@ -35,7 +20,6 @@ const Search = props => {
           className="text-center mb-3"
           onChange={(e) => setAuthorInput(e.target.value)}
         />
->>>>>>> 6d3d61877e01f0c33a98401e4ac01797a04a461d
       </Form>
 
       <Filters input={input} author={authorInput} />
