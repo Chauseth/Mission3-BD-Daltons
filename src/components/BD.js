f75fc55cc50166e9b50e249cc86217d75d4be7dd
Chauseth <alexandre.chiquet@hotmail.com>
2021-01-20 15:41:35 +0100

    Revert "Composant panier"
    This reverts commit 27ce44c374762f8306e0cf7c4c5720f05d27ecb0.

--- src/components/BD.js
+++ src/components/BD.js
@@ -4,12 +4,13 @@ import Panier from "./Panier"
 import Button from "react-bootstrap/Button";
 import { FaCartArrowDown } from "react-icons/fa";
 import { series } from "./data/series";
+import Modale from "./Modale";
+import VignetteModale from "./VignetteModale";
 
-
-const BD = (props) => {
+const BD = props => {
   const pathBD = "./img/albumsMini/";
-  const {onAdd} = props;
-  const nameSerie = series.find((serie) => {
+
+  const nameSerie = series.find(serie => {
     if (serie.id === props.idSerie) {
       return true;
     }
@@ -23,12 +24,20 @@ const BD = (props) => {
   return (
     <Card className="mt-5">
       <Card.Img variant="top" src={cleanPath} />
-      <Card.Body className="bg-dark">
-        <Card.Title>{props.titre}</Card.Title>
-        <Card.Text>{props.prix} €</Card.Text>
+      <Card.Body
+        id="card-body"
+        className="bg-dark d-flex flex-column  align-items-md-center"
+      >
+        <Card.Title id="set-title">{props.titre}</Card.Title>
+        <Card.Text id="set-text">{props.prix} €</Card.Text>
       </Card.Body>
+<<<<<<< HEAD
       <Card.Footer>
         <Button id="buttonBuy" onClick={() => onAdd(props)} >
+=======
+      <Card.Footer id="set-carFooter">
+        <Button id="buttonBuy">
+>>>>>>> 083d0d37074ab9c318ac769b90d40b0178c804e9
           <FaCartArrowDown />{" "}
         </Button>
       </Card.Footer>
